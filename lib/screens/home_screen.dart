228f7ce4c006c3cb659ce0bdc662e654e303b4fd
Gansputra <ganangsetyaputra2@gmail.com>
2026-03-04 21:00:53 +0700

    menghilangkan appbar demo

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,9 +4,7 @@ import '../models/vocab.dart';
 import '../services/vocab_service.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key, required this.title});
-
-  final String title;
+  const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -34,11 +32,6 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-        centerTitle: true,
-      ),
       body: FutureBuilder<List<Vocab>>(
         future: _vocabFuture,
         builder: (context, snapshot) {
